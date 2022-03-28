29d377cfe01a01ee980efec306e1d2d82ed842cc
aurelius <petrovsnik@gmail.com>
2022-03-28 22:34:29 +0300

    Добавьте файлы проекта.

---
+++ n-arrays/Program.cs
@@ -0,0 +1,69 @@
+﻿bool check = true;
+int strings, columns;
+do
+{
+    Console.Write("\nvvedite chislo strok: ");
+    check = int.TryParse(Console.ReadLine(),out strings);
+    if (!check)
+    {
+        Console.WriteLine("vvedeno neopoznannoe chislo, vvedite povtorno!");
+        Console.ReadKey();
+        continue;
+    }
+    else if (strings <= 0)
+    {
+        Console.WriteLine("vvedeno otricatel'noe chislo ili 0, vvedite povtorno!");
+        Console.ReadKey();
+        continue;
+    }
+        else
+        {
+            Console.WriteLine($"chislo strok {strings} prinyato.");
+            break;
+        }
+}
+while (true) ;
+
+do
+{
+    Console.Write("vvedite chislo stolbcov: ");
+    check = int.TryParse(Console.ReadLine(), out columns);
+    if (!check)
+    {
+        Console.WriteLine("\nvvedeno neopoznannoe chislo, vvedite povtorno!");
+        Console.ReadKey();
+        continue;
+    }
+    else if (columns <= 0)
+    {
+        Console.WriteLine("vvedeno otricatel'noe chislo ili 0, vvedite povtorno!");
+        Console.ReadKey();
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"chislo strok {columns} prinyato.");
+        break;
+    }
+}
+while (true);
+
+Console.Clear();
+Console.WriteLine($"matrica {strings} na {columns}:\n");
+
+int[,] matrix = new int[strings,columns];
+Random r = new Random();
+int sum = 0;
+
+for (int i = 0; i < strings; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = r.Next(-50,50);
+        Console.Write($"{matrix[i,j]} ");
+        sum+= matrix[i,j];
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"summa elementov matrici: {sum}");
---
+++ posledovatelnost/Program.cs
@@ -0,0 +1,59 @@
+﻿bool check;
+int len;
+
+do
+{
+    Console.Write("\nchislo elementov posledovatelnosti: ");
+    check = int.TryParse(Console.ReadLine(), out len);
+    if (!check)
+    {
+        Console.WriteLine("vvedeno neopoznannoe chislo, vvedite povtorno!");
+        //Console.ReadKey();
+        continue;
+    }
+    else if (len <= 0)
+    {
+        Console.WriteLine("vvedeno otricatel'noe chislo ili 0, vvedite povtorno!");
+        //Console.ReadKey();
+        continue;
+    }
+    else
+    {
+        Console.WriteLine($"chislo strok {len} prinyato.");
+        break;
+    }
+}
+while (true);
+
+Console.Clear();
+
+Console.Write($"\nchislo elementov posledovatelnosti: {len}\n\n");
+
+int[] posl = new int[len];
+
+for (int i = 0; i < len; i++)
+{
+    do
+    {
+        Console.Write($"{i+1}-i element: ");
+        check = int.TryParse(Console.ReadLine(), out posl[i]);
+        if (!check)
+        {
+            Console.WriteLine("vvedeno nekorrektnoe chislo, vvedite povtorno!");
+            Console.ReadKey();
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    }
+    while (true);
+}
+
+Console.Clear();
+Console.WriteLine("posledovatelnost' imeet vid:");
+foreach (int i in posl)
+{
+    Console.Write($"{i} ");
+}
