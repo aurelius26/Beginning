9f65d209cb602bc686ba34756ac0ca23b824d172
aurelius <petrovsnik@gmail.com>
2022-04-03 12:09:16 +0300

    03.04.2022_01

---
+++ StreamRW/Program.cs
@@ -0,0 +1,18 @@
+﻿var dirs = new DirectoryInfo(@"C:\").GetDirectories();
+
+StreamWriter sw = new StreamWriter("SW.txt");
+
+foreach (DirectoryInfo dir in dirs)
+{
+    sw.WriteLine(dir.Name);
+    Console.WriteLine($">>{dir.FullName}");
+}
+sw.Close();
+
+using (StreamReader sr = new StreamReader("SW.txt"))
+{
+    while (!sr.EndOfStream)
+    {
+        Console.WriteLine(sr.ReadLine());
+    }
+}
